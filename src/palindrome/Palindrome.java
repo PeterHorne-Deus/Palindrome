4dd4696a819310442a83a60f67152750a2920748
Peter <Peter@192.168.2.16>
2020-02-22 19:07:47 -0500

    Final commit of Palindrome

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Peter Horne-Deus
+ * The tests for palindroms
+ * calendarCulminating.java
+ * Feburary 22, 2020
  */
 
 package palindrome;
@@ -18,17 +19,23 @@ public class Palindrome {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Variables
         String input,forward;
         String reverse = "";
         
+        //Getting user input
         input = JOptionPane.showInputDialog("A Palinrom is a word that is the same forward and backwards. Plase enter a string ");
+        
+        //Setting all letters in string to lower case 
         forward = input.toLowerCase();
         
+        //Setting the reverse of the string previously entered
         for (int i = input.length()-1 ; i >= 0; i--){
             reverse = reverse + input.charAt(i);
         }
         reverse = reverse.toLowerCase();
         
+        //Checking if the reverse eqauals the forward and is therefore a plaindrome
         if(forward.equals(reverse)){
             JOptionPane.showMessageDialog(null, "This is a palindrom");
         }
